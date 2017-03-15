214b240e3ec94cc942580a40271082f4d044552e
yulelee <yulelee@stanford.edu>
2017-03-15 14:06:44 -0700

    parse date for search resutls

--- server/database.js
+++ server/database.js
@@ -9,17 +9,45 @@ var Message = require('../schema/message.js');
 
 var dataBaseHandler = {};
 
+var parseDate = function(buckets, callback) {
+	async.each(buckets, function(bucket, finishOneBucket) {
+		bucket.date_time = new Date(bucket.date_time).toLocaleString('en-US', {
+			weekday: 'short',
+			year: 'numeric',
+			month: 'short',
+			day: 'numeric',
+			hour12: true, 
+			hour: "2-digit", 
+			minute: "2-digit"
+		});
+		finishOneBucket();
+	}, function(err) {
+		if (err) { res.status(400).send('Error attaching date strings, final.'); }
+		else { callback(); }
+	});
+};
+
 dataBaseHandler.searchSuidHistory = function(req, res) {
 	Bucket.find({studentSuids: req.query.suid}, function(err, buckets) {
 		if (err) { res.status(400).send('Error finding buckets.'); } 
-		else { res.status(200).send(JSON.stringify(buckets));}
+		else { 
+			var buckets = JSON.parse(JSON.stringify(buckets));
+			parseDate(buckets, function() {
+				res.status(200).send(JSON.stringify(buckets));
+			});
+		}
 	});
 };
 
 dataBaseHandler.searchDescriptionKeyWordsHistory = function(req, res) {
 	Bucket.find({description: {$regex: req.query.keyword, $options: 'si'}}, function(err, buckets) {
 		if (err) { res.status(400).send('Error finding buckets.'); }
-		else { res.status(200).send(JSON.stringify(buckets)); }
+		else { 
+			var buckets = JSON.parse(JSON.stringify(buckets));
+			parseDate(buckets, function() {
+				res.status(200).send(JSON.stringify(buckets));
+			});
+		}
 	});
 };
 
