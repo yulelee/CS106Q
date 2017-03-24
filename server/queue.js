a3b0a58435b6f24923ad439dc4e0f3da00a7c2e9
yulelee <yulelee@stanford.edu>
2017-03-24 00:40:10 -0700

    queue module updated to promise

--- server/queue.js
+++ server/queue.js
@@ -9,147 +9,134 @@ var Message = require('../schema/message.js');
 var DatetimeUtils = require('./datetimeUtils.js');
 
 var messageHandler = require('./message.js');
+var GeneralUtil = require('./util.js');
 
 var queueHandler = {};
 
 var averageHelpingTime = 20.0; // 20 min
 
+var computeRemainingTime = function(waitingBucketCount) {
+	return new Promise(function(resolve, reject) {
+		SL.count({logged_in_sessionId: {$ne : undefined}}).exec().then(function(loggedInSlCount) {
+			var waitingTime = waitingBucketCount * averageHelpingTime / Math.max(1, loggedInSlCount); // time for the buckets still waiting
+			Bucket.find({helperSL: {$ne : undefined}, solved: false}).sort({helpStartTime: -1}).limit(1).exec().then(function(newestStartedHelpedBucket) {
+				var needTime = newestStartedHelpedBucket.length === 0 ? 0 : (averageHelpingTime - ((new Date()) - newestStartedHelpedBucket[0].helpStartTime) / 1000 / 60); // time needed to end this one
+				needTime = Math.max(0, needTime);
+				waitingTime += needTime;
+				resolve(waitingTime);
+			});
+		}).catch(function(err) { reject(err); });
+	});
+};
+
 queueHandler.getCurInfo = function(req, res) {
 	var info = {};
-	var curDate = new Date();
-	Bucket.count({helperSL: undefined}, function(err, waitingBucketCount) {
-		if (err) { res.status(400).end('Error counting unresolved buckets'); }
-		else {
-			info.waitingBucketCount = waitingBucketCount;
-			SL.count({logged_in_sessionId: {$ne : undefined}}, function(err, loggedInSlCount) {
-				if (err) { res.status(400).end('Error counting sls'); }
-				else {
-					info.waitingTime = waitingBucketCount * averageHelpingTime / Math.max(1, loggedInSlCount); // time for the buckets still waiting 
-					Bucket.find({helperSL: {$ne : undefined}, solved: false}).sort({helpStartTime: -1}).limit(1).exec(function(err, newestStartedHelpedBucket) {
-						var needTime = newestStartedHelpedBucket.length === 0 ? 0 : (averageHelpingTime - ((new Date()) - newestStartedHelpedBucket[0].helpStartTime) / 1000 / 60); // time needed to end this one
-						needTime = Math.max(0, needTime);
-						info.waitingTime += needTime;
-						res.status(200).end(JSON.stringify(info));
-					});
-				}
-			});
-		}
+	Bucket.count({helperSL: undefined}).exec().then(function(waitingBucketCount) {
+		info.waitingBucketCount = waitingBucketCount;
+		computeRemainingTime(waitingBucketCount).then(function(waitingTime) {
+			info.waitingTime = waitingTime;
+			res.status(200).send(JSON.stringify(info));
+		});
+	}).catch(function(err) {
+		res.status(400).send(err);
+	});
+};
+
+// one person cannot have more than one on-going buckets
+var validateExistingBucket = function(suid) {
+	return new Promise(function(resolve, reject) {
+		Bucket.findOne({studentSuids: suid}, "_id").exec().then(function(bucket) {
+			if (bucket) { reject('Error checking existing SUID.'); }
+			else { resolve(); }
+		});
 	});
 };
 
 queueHandler.insertNew = function(req, res) {
-	Bucket.findOne({_id: req.body._id}, function(err, bucket) {
-	    if (err) {res.status(400).end('Error checking existing SUID.');}
-	    else {
-	        if (!bucket) {res.status(400).end('SUID not existing.');}
-	        else {
-	            bucket.students.push(req.body.studentName);
-	            bucket.studentSuids.push(req.body.suid);
-	            bucket.save(function(err, bucket) {
-	                if (err) {res.status(400).end('Error saving bucket.');}
-	                else {
-	                	clientList.broadcastChange();
-	                	res.status(200).end(JSON.stringify(bucket));
-	                }
-	            });
-	        }
-	    }
+	validateExistingBucket(req.body.suid)
+	.then(() => Bucket.findOne({_id: req.body._id}).exec())
+	.then((bucket) => new Promise(function(resolve, reject) {
+		bucket.students.push(req.body.studentName);
+		bucket.studentSuids.push(req.body.suid);
+		bucket.save().then(resolve);
+	})).then(function() {
+		res.status(200).end('Success.');
+		clientList.broadcastChange();
+	}).catch(function(err) {
+		res.status(400).send(err);
 	});
 };
 
 queueHandler.putNew = function(req, res) {
-	Bucket.findOne({studentSuids: req.body.suid}, "_id", function(err, bucketExisted) {
-	    if (err) {res.status(400).end('Error checking existing SUID.');}
-	    else {
-	        if (bucketExisted) {res.status(400).end('SUID already existed.');}
-	        else {
-	            var bucket = new Bucket({ 
-	            	type: req.body.type,
-	            	description: req.body.description, 
-	            	class: req.body.class,
-	            	students: [req.body.studentName],
-	            	studentSuids: [req.body.suid],
-	            	helperSL: undefined,
-	            	position: req.body.position
-	            });
-	            bucket.save(function(err, bucket) {
-	                if (err) {res.status(400).end('Error saving new bucket.');}
-	                else {
-	                	clientList.broadcastChange();
-	                	res.status(200).end(JSON.stringify(bucket));
-	                }
-	            });
-	        }
-	    }
+	validateExistingBucket(req.body.suid).then(function() {
+		var bucket = new Bucket({ 
+			type: req.body.type,
+			description: req.body.description, 
+			class: req.body.class,
+			students: [req.body.studentName],
+			studentSuids: [req.body.suid],
+			position: req.body.position
+		});
+		bucket.save().then(function() {
+			res.status(200).send('Success.');
+			clientList.broadcastChange();
+		});
+	}).catch(function(err) {
+		res.status(400).send(err);
 	});
 };
 
-queueHandler.attachSL = function(buckets, callback) {
-	async.each(buckets, function(bucket, finishOneBucket) {
-		SL.findOne({_id: bucket.helperSL}, function(err, sl) {
-			if (err) { callback('Error finding sl.'); }
-			else {
+queueHandler.attachSL = function(buckets) {
+	return new Promise(function(resolve, reject) {
+		async.each(buckets, function(bucket, finishOneBucket) {
+			SL.findOne({_id: bucket.helperSL}).exec().then(function(sl) {
 				bucket.helperSL = JSON.parse(JSON.stringify(sl));
 				finishOneBucket();
-			}
+			});
+		}, function(err) {
+			if (err) { reject(err); }
+			else { resolve(buckets); }
 		});
-	}, function(err) {
-		if (err) { callback('Error attaching sl, final.'); }
-		else { callback(); }
 	});
 };
 
 var assembleSolved = function() {
 	return new Promise(function(resolve, reject) {
-		Bucket.find({solved: true}).sort({'helpEndTime': -1}).limit(20).exec(function(err, solvedBuckets) {
-			if (err) { reject('Error retrieving buckets.'); }
-			else {
-				solvedBuckets = JSON.parse(JSON.stringify(solvedBuckets));
-				DatetimeUtils.parseTime(solvedBuckets, function(err) {
-					if (err) { reject(err); }
-					else {
-						queueHandler.attachSL(solvedBuckets, function(err) {
-							if (err) { reject(err); }
-							else { resolve(solvedBuckets); }
-						});
-					}
-				});
-			}
+		Bucket.find({solved: true}).sort({'helpEndTime': -1}).limit(20).exec()
+		.then(GeneralUtil.parseCopy)
+		.then(DatetimeUtils.parseDate)
+		.then(queueHandler.attachSL)
+		.then(function(buckets) {
+			resolve(buckets);
+		}).catch(function(err) {
+			reject(err);
 		});
 	});
 };
 
 var assembleSolving = function() {
 	return new Promise(function(resolve, reject) {
-		Bucket.find({solved: false, helperSL: {$ne: undefined}}).sort({'helpStartTime': -1}).exec(function(err, solvingBuckets) {
-			if (err) { reject('Error retrieving buckets.'); }
-			else {
-				solvingBuckets = JSON.parse(JSON.stringify(solvingBuckets));
-				DatetimeUtils.parseTime(solvingBuckets, function(err) {
-					if (err) { reject(err); }
-					else {
-						queueHandler.attachSL(solvingBuckets, function(err) {
-							if (err) { reject(err); }
-							else { resolve(solvingBuckets); }
-						});
-					}
-				});
-			}
+		Bucket.find({solved: false, helperSL: {$ne: undefined}}).sort({'helpStartTime': -1}).exec()
+		.then(GeneralUtil.parseCopy)
+		.then(DatetimeUtils.parseTime)
+		.then(queueHandler.attachSL)
+		.then(function(buckets) {
+			resolve(buckets);
+		}).catch(function(err) {
+			reject(err);
 		});
 	});
 };
 
 var assemblePending = function() {
 	return new Promise(function(resolve, reject) {
-		Bucket.find({solved: false, helperSL: undefined}).sort({'date_time': 1}).exec(function(err, pendingBuckets) {
-			if (err) { reject('Error retrieving buckets.'); }
-			else {
-				pendingBuckets = JSON.parse(JSON.stringify(pendingBuckets));
-				DatetimeUtils.parseTime(pendingBuckets, function(err) {
-					if (err) { reject(err); }
-					else { resolve(pendingBuckets); }
-				});
-			}
+		Bucket.find({solved: false, helperSL: undefined}).sort({'date_time': 1}).exec()
+		.then(GeneralUtil.parseCopy)
+		.then(DatetimeUtils.parseTime)
+		.then(function(buckets) {
+			resolve(buckets);
+		}).catch(function(err) {
+			reject(err);
 		});
 	});
 };
@@ -179,7 +166,7 @@ var markBucketAsResolved = function(bucket_id) {
 var markSlAsFree = function(sl_id) {
 	return new Promise(function(resolve, reject) {
 		SL.findOne({currently_helping: {$ne: undefined}, _id: sl_id}).exec().then(function(sl) {
-			if (sl.currently_helping === undefined) { reject('You are not helping.'); }
+			if (!sl.currently_helping) { reject('You are not helping.'); }
 			else {
 				sl.currently_helping = undefined; // mark sl free
 				sl.save().then(function() { resolve(); });
@@ -191,10 +178,10 @@ var markSlAsFree = function(sl_id) {
 queueHandler.solveBucket = function(req, res) {
 	markBucketAsResolved(req.body.bucket_id)
 	.then(markSlAsFree(req.session.sl_id))
-	.then(messageHandler.createNewMessage(req.session.sl_id, req.body.message, req.body.bucket_id))
 	.then(function() {
 		res.status(200).send('Success.');
 		clientList.broadcastChange();
+		messageHandler.createNewMessage(req.session.sl_id, req.body.message, req.body.bucket_id);
 	}).catch(function(err) {
 		res.status(400).send(err);
 	});
@@ -204,7 +191,7 @@ queueHandler.solveBucket = function(req, res) {
 var markBucketAsClean = function(bucket_id) {
 	return new Promise(function(resolve, reject) {
 		Bucket.findOne({_id: bucket_id}).exec().then(function(bucket) {
-			if (bucket.helperSL === undefined) { reject('Error bucket not marked as being helped.'); }
+			if (!bucket.helperSL) { reject('Error bucket not marked as being helped.'); }
 			else {
 				bucket.helperSL = undefined;
 				bucket.helpStartTime = undefined; // erase the starting time of this help
